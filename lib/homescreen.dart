bfe67f08b7cf1078aa88e83a7b5622df7c8aee4a
gav974ordi1 <eclapier@msn.com>
2022-01-17 12:16:16 +0400

    ajout de variable type icon dans un liste et et creation d'un fonction refactorisé

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -9,82 +9,87 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<String> resultIcone =[];
+  final Icon checkIcon = const Icon(Icons.check,color:Colors.green);
+  final Icon closeIcon = const Icon(Icons.close,color:Colors.red);
+  var resultIcone = <Icon>[];
 
-
+  void addItems (item){
+    setState(() {
+      resultIcone.add(item);
+    }
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
 
     return Scaffold(
-
     backgroundColor: Colors.grey.shade900,
       body: SafeArea(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              const Expanded(
+          children: [
+            const Expanded(
               flex: 5,
-                child:Padding(
-                  padding: EdgeInsets.symmetric(vertical:15.0, horizontal: 10.00),
-                  child: Center(
-                    child: Text(
-                      'Question',
-                      style: TextStyle(
+              child:Padding(
+                padding: EdgeInsets.symmetric(vertical:15.0, horizontal: 15.00),
+                child: Center(
+                  child: Text('Question',
+                    style: TextStyle(
                         color: Colors.white,
-                        fontSize: 25,
-                      ),
-                      textAlign: TextAlign.center,
+                        fontSize: 25
                     ),
+                    textAlign: TextAlign.center,
                   ),
                 ),
               ),
-              Expanded(
-
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical:15.0 ,horizontal: 15.0),
-                  child: TextButton(
-                    style: TextButton.styleFrom(
-                      backgroundColor: Colors.green,
-                      onSurface: Colors.white,
-                    ),
-                    onPressed: null,
-                    child: const Text("Vrai",
-                      style:TextStyle(
-                          color:Colors.white70,
-                          fontSize:20
-                      ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical:15.0 ,horizontal: 15.0),
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.green,
+                    onSurface: Colors.white,
                   ),
-              ),
-                ),
-              ),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(10, 15, 15, 15),
-                  child: TextButton(
-                    style: TextButton.styleFrom(
-                      backgroundColor: Colors.red,
-                      onSurface: Colors.white,
-                    ),
-                    onPressed: null,
-                    child: const Text("Faux",style:TextStyle(
+                  onPressed: (){
+                   addItems(checkIcon);
+                    },
+                  child: const Text("Vrai",
+                    style:TextStyle(
                         color:Colors.white70,
-                        fontSize:20),
+                        fontSize:20
                     ),
                   ),
                 ),
               ),
-              Row(
-                  children: [
-                    Icon(Icons.check,color:Colors.green),
-                     Icon(Icons.close,color:Colors.red),
-    ],
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(10, 15, 15, 15),
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.red,
+                    onSurface: Colors.white,
+                  ),
+                  onPressed: (){
+                    addItems(closeIcon);
+                  },
+                  child: const Text("Faux",style:TextStyle(
+                      color:Colors.white70,
+                      fontSize:20
+                    ),
+                  ),
                 ),
-                          ],
-          ),
+              ),
+            ),
+            Row(
+              children: resultIcone,
+            )
+          ],
+        ),
       ),
-      );
-
+    );
   }
 }
 
